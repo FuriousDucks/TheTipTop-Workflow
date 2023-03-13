c835f1f122d3f84bd597cc6557a6241cfbc20352
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2023-03-13 22:01:39 +0100

    Add server backup

---
+++ backup/dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set the name of the directory to archive
+DIR_NAME="/var/www/"
+
+# Stop the Docker container before starting the backup
+
+# Set the name of the archive file
+ARCHIVE_NAME="thetiptop-server-$(date +"%Y%m%d%H%M%S").tar.gz"
+
+# Create the archive
+tar -czf "$ARCHIVE_NAME" "$DIR_NAME"
+
+# Use rclone to upload the archive to Google Drive
+rclone copy "$ARCHIVE_NAME" remote:server-backups
+
+# Start the Docker container again
+
+# Delete the archive
+rm "$ARCHIVE_NAME"
